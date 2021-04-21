11e5137882fd9ad7bc35d1d0ec540c38444cefc1
anasmasti <anasmasti@hotmail.com>
2021-04-21 14:03:17 +0000

    Set up access to socket

--- routes/home.js
+++ routes/home.js
@@ -4,8 +4,7 @@ var path = require('path');
 
 //Call the index file on init app
 router.get('/', (req, res) => {
-    res.sendFile(path.resolve('public/index.html'));
-    // res.send('Sahm back-end work :)');
+    res.send('Sahm back-end work :)');
 })
 
 module.exports = router;
